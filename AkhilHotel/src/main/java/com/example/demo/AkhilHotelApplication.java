94833abe958d549de8d96c527cf15932e5de0762
A00443079 <akhil.roy@smu.ca>
2021-07-02 23:12:59 +0530

    Installed Everything, but not yet started coding

---
+++ AkhilHotel/src/main/java/com/example/demo/AkhilHotelApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkhilHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkhilHotelApplication.class, args);
+	}
+
+}
